e9840583e263d6b3acc68c3c67333876b7bee6bf
Jihan Chao <716833+onelittlefish@users.noreply.github.com>
2015-03-22 14:18:25 -0700

    Load books on login

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -12,13 +12,8 @@ define(function(require) {
 	var LibraryInfo = require('models/LibraryInfo');
 	var RootView = require('views/RootView');
 
-	// var bookList = new Book.collection([
-	// 	new Book({title: 'A', author: 'B', libraryInfo: new LibraryInfo({name: "SCLib", isAvailable: true, section: "new"})}),
-	// 	new Book({title: 'C', author: 'D'}),
-	// 	new Book({title: 'D'})
-	// ]);
-
 	var bookList = new Book.collection();
+	bookList.fetch();
 
 	var rootView = new RootView({
 		collection: bookList,
